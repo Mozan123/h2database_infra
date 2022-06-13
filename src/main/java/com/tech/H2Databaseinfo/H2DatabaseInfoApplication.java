e7aeda6b08390cba0a42d981666992afa2c53b71
Mozan123 <moznyousif@gmail.com>
2022-06-13 13:22:09 -0400

    init project

---
+++ src/main/java/com/tech/H2Databaseinfo/H2DatabaseInfoApplication.java
@@ -0,0 +1,13 @@
+package com.tech.H2Databaseinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DatabaseInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DatabaseInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/H2Databaseinfo/H2DatabaseInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.H2Databaseinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DatabaseInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
